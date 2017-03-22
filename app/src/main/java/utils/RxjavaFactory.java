7033242cc8abd4af34c97143e3a22a6a8b0ca830
mrqatom <very_mrq@sina.com>
2017-03-22 23:28:10 +0800

    retrofit/rxjava_tool

---
+++ app/src/main/java/utils/RxjavaFactory.java
@@ -0,0 +1,36 @@
+package utils;
+
+//import io.reactivex.Observable;
+//import io.reactivex.ObservableSource;
+//import io.reactivex.ObservableTransformer;
+//import io.reactivex.android.schedulers.AndroidSchedulers;
+//import io.reactivex.functions.Function;
+//import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by atom on 2017/3/22.
+ * RxJava常用transformer
+ */
+
+public class RxjavaFactory {
+//
+//    public static class HttpResponseFunc<T> implements Function<Throwable, Observable<T>> {
+//        @Override
+//        public Observable<T> apply(Throwable throwable) throws Exception {
+//            return Observable.error(ExceptionUtils.handleException(throwable));
+//        }
+//    }
+//
+//    public static <T> ObservableTransformer<T, T> io_main_trasformer(){
+//        return new ObservableTransformer<T, T>() {
+//            @Override
+//            public ObservableSource<T> apply(Observable<T> upstream) {
+//                return upstream.subscribeOn(Schedulers.io())
+//                        .observeOn(AndroidSchedulers.mainThread());
+//            }
+//        };
+//    }
+
+
+
+}
